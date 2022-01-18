ecdaeee4da7473f90908d64a0b4ec8d543f0f390
Fahad Flava <94212358+Siesta012@users.noreply.github.com>
2022-01-18 21:07:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ global.tum2 = fs.readFileSync('./src/emror.jpeg')
 global.thumb = fs.readFileSync('./src/thumb.jpeg')
 global.thumb2 = fs.readFileSync('./src/emror.jpeg')
 global.owner = ['6285871388418'] // Put your number here
-global.mods = ['6285693120588'] // Want some help?
+global.mods = ['6285871388418'] // Want some help?
 global.prems = ['6287842147162','6285893120588','6285656615441'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
